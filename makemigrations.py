ab75db4fd6ed3e9c5ea44c6953fd1fdb25cb8948
makarov <makarov@tlpt.ru>
2023-07-04 01:02:28 +0400

    something

--- makemigrations.py
+++ makemigrations.py
@@ -4,8 +4,24 @@ from migrate.versioning.schema import Table, Column
 from database.db import session
 from models.models import Wishes
 
-row = "ALTER TABLE wishes ADD COLUMN user_id INTEGER"
-row2 = "ALTER TABLE wishes ADD COLUMN date_of_wish DATE"
+row = """
+ALTER TABLE wishes
+ADD COLUMN user_id INTEGER,
+ADD COLUMN date_of_wish DATE;
+
+
+UPDATE wishes
+SET user_id = 0, -- Specify the appropriate default value
+    date_of_wish = '1970-01-01'; -- Specify the appropriate default date
+
+
+ALTER TABLE wishes
+DROP COLUMN name_of_user;
+
+
+ALTER TABLE wishes
+RENAME TO updated_wishes;"""
+
 
 wishes = session.query(Wishes).all()
 
@@ -15,4 +31,4 @@ for i in wishes:
     session.commit()
 
 session.execute(row)
-session.execute(row2)
+
